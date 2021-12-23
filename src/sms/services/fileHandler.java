93c34df30ec08715c10b116b5de3bea1c72c9841
MuhammadAli511 <mohammadali417@outlook.com>
2021-12-23 17:19:03 +0500

    File Reading

--- src/sms/services/fileHandler.java
+++ src/sms/services/fileHandler.java
@@ -1,25 +1,24 @@
 package sms.services;
-import java.io.*;
-import java.util.*;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 import sms.model.Account;
 
-public class fileHandler extends persistenceHandler{
-    public void checkUser(Account acc)
-    {
-        try{
-            BufferedReader in = new BufferedReader(new FileReader("Account.txt"));
-            String str;
-            while ((str = in.readLine())!= null) {
-                System.out.println(str);
-                String[] data_Arr = str.split(",");
-                if (data_Arr[1].equals(acc.getEmail()) == true && data_Arr[2].equals(acc.getPassword()) == true) {
-                    System.out.println("Logged In");
-                    break;
-                }
-                System.out.println("Clicked");
-            }
-            in.close();
-        } catch(IOException error){}
-    }
 
+
+public class fileHandler extends persistenceHandler {
+    public void checkUser(Account acc) {
+        try {
+            File myObj = new File("Account.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
